cc87a72ed2c1a2d187890ec02e26dc6cfb357fb0
teto0525 <86487948+teto0525@users.noreply.github.com>
2021-07-29 11:34:09 +0900

    FSW

---
+++ Assets/02. Scripts/PetCtrl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetCtrl : MonoBehaviour
+{
+
+    public float speed = 4;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //1. Player 방향 구하기
+        Vector3 dir = target.transform.position - this.transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+    }
+}
