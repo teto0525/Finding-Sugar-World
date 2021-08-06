e9008be1f44f2028bb98ab68a5f6f11db5c277af
jeedd95 <80206325+jeedd95@users.noreply.github.com>
2021-08-06 17:30:51 +0900

    .

--- Assets/JHW/scripts/hp.cs
+++ Assets/JHW/scripts/hp.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class hp : MonoBehaviour
 {
+
+    public float moveSpeed = 10;
+
     [SerializeField]
     private Slider hpbar;
 
@@ -23,12 +26,58 @@ public class hp : MonoBehaviour
 
     void Update()
     {
-        currHP -= 1;
+       
         HandleHP();
     }
 
     void HandleHP()
     {
         hpbar.value = Mathf.Lerp(hpbar.value, (float)currHP / (float)iniHP, Time.deltaTime * 10);
+    }
+
+    //===================================================================================
+    void OncollisionEnter(Collider coll)
+    {
+
+        // 만약 빨간약 먹으면
+        if (coll.gameObject.CompareTag("Potion_Purple"))
+        {
+
+            //hp가 회복된다
+            currHP += 30.0f;
+        }
+    }
+    // Enemy 피격
+    private void OnTriggerEnter(Collision coll)
+    {
+        //충돌한 collider가 Enemy이면 HP 차감
+        if (currHP >= 0.0f && coll.collider.CompareTag("Enemy"))
+        {
+            // 충격 모션
+            anim.SetTrigger("gotHit");
+            this.transform.Translate(Vector3.back * 10.0f * moveSpeed * Time.deltaTime);
+
+            currHP -= 10.0f;
+        }
+
+        if (currHP >= 0.0f && coll.collider.CompareTag("Boss"))
+        {
+            BossAttack();
+        }
+        if (currHP <= 0.0f)
+        {
+            PlayerDie();
+        }
+
+    }
+
+    // Boss
+    void BossAttack()
+    {
+        anim.SetTrigger("gotHit");
+        this.transform.Translate(Vector3.back * 10.0f * moveSpeed * Time.deltaTime);
+
+        currHP -= 30.0f;
+        Debug.Log("Player hp = {iniHP - currHp}");
     }
 }
