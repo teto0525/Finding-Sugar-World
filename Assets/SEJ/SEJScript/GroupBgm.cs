31fb18b0fbeacf3c51d8d02ec95bb89c1a7cc2cc
eileenshin <87408515+eileenshin@users.noreply.github.com>
2021-08-07 11:42:06 +0900

    .

---
+++ Assets/SEJ/SEJScript/GroupBgm.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupBgm : MonoBehaviour
+{
+    public AudioClip audioChangeSword;
+    public AudioClip audioAttackObs;
+    public AudioClip audioAttackBoss;
+    public AudioClip audioEatItem;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/SEJ/SEJScript/SugarWorldSFX.cs
+++ Assets/SEJ/SEJScript/SugarWorldSFX.cs
@@ -6,7 +6,7 @@ public class SugarWorldSFX : MonoBehaviour
 {
     public AudioClip swAudio;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         AudioSource audio = GetComponent<AudioSource>();
     }
